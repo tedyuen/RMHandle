9e0e3ce7ebfb2a4aed1b9bab982aec3f4aad12b3
TedYuen <ted.yuan@wojiawang.com.cn>
2016-06-29 13:26:00 +0800

    bug

--- app/src/main/java/cn/com/reachmedia/rmhandle/utils/ImageUtils.java
+++ app/src/main/java/cn/com/reachmedia/rmhandle/utils/ImageUtils.java
@@ -230,12 +230,13 @@ public class ImageUtils {
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         image.compress(Bitmap.CompressFormat.JPEG, 80, baos);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        int options = 100;
+        int options = 80;
         while ( baos.toByteArray().length / 1024>100) {  //循环判断如果压缩后图片是否大于100kb,大于继续压缩
             baos.reset();//重置baos即清空baos
             image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
             options -= 10;//每次都减少10
         }
+        image.recycle();
         ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
         Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
         return bitmap;
