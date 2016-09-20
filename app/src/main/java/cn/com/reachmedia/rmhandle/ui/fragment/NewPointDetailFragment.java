f0f23e3dd0a66377788232766be7335dd18cd64f
TedYuen <tedyuen.goo@gmail.com>
2016-09-20 16:58:59 +0800

    Finish customer's data.

--- app/src/main/java/cn/com/reachmedia/rmhandle/ui/fragment/NewPointDetailFragment.java
+++ app/src/main/java/cn/com/reachmedia/rmhandle/ui/fragment/NewPointDetailFragment.java
@@ -1,15 +1,29 @@
 package cn.com.reachmedia.rmhandle.ui.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import cn.com.reachmedia.rmhandle.R;
+import cn.com.reachmedia.rmhandle.app.AppSpContact;
+import cn.com.reachmedia.rmhandle.bean.PointBean;
+import cn.com.reachmedia.rmhandle.db.utils.PointBeanDbUtil;
+import cn.com.reachmedia.rmhandle.db.utils.PointWorkBeanDbUtil;
+import cn.com.reachmedia.rmhandle.model.PointListModel;
 import cn.com.reachmedia.rmhandle.ui.base.BaseToolbarFragment;
+import cn.com.reachmedia.rmhandle.ui.view.Line3ImageLayout;
 import cn.com.reachmedia.rmhandle.ui.view.LineImageLayout;
+import cn.com.reachmedia.rmhandle.utils.StringUtils;
 
 /**
  * Created by tedyuen on 16-9-19.
@@ -20,10 +34,20 @@ public class NewPointDetailFragment extends BaseToolbarFragment {
     @Bind(R.id.line_image_1)
     LineImageLayout lineImage1;
 
+    @Bind(R.id.tv_action_time)
+    TextView tvActionTime;
+    @Bind(R.id.tv_cname)
+    TextView tvCname;
+    @Bind(R.id.wb_memo)
+    WebView wbMemo;
+    @Bind(R.id.ll_cust_photo)
+    Line3ImageLayout ll_cust_photo;
+
+
+
     public static NewPointDetailFragment newInstance() {
         NewPointDetailFragment fragment = new NewPointDetailFragment();
         Bundle args = new Bundle();
-//        args.putParcelable(AppParamContact.PARAM_KEY_MODEL, model);
         fragment.setArguments(args);
         return fragment;
     }
@@ -32,9 +56,36 @@ public class NewPointDetailFragment extends BaseToolbarFragment {
     }
 
 
+    private PointBeanDbUtil pointBeanDbUtil;
+    private PointWorkBeanDbUtil pointWorkBeanDbUtil;
+    private PointListModel pointListModel;//缓存列表数据
+    private PointBean bean;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pointBeanDbUtil = PointBeanDbUtil.getIns();
+        pointWorkBeanDbUtil = PointWorkBeanDbUtil.getIns();
+        String communityId = mSharedPreferencesHelper.getString(AppSpContact.SP_KEY_INDEX_COMMUNITID);
+        String tempStartTime = mSharedPreferencesHelper.getString(AppSpContact.SP_KEY_INDEX_STARTTIME);
+        String dataJson = mSharedPreferencesHelper.getString(communityId+"_"+tempStartTime);
+        String workId = mSharedPreferencesHelper.getString(AppSpContact.SP_KEY_WORK_ID);
+        String pointId = mSharedPreferencesHelper.getString(AppSpContact.SP_KEY_POINT_ID);
+        if(!StringUtils.isEmpty(dataJson)) {
+            Gson gson = new Gson();
+            try {
+                pointListModel = gson.fromJson(dataJson, PointListModel.class);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                getActivity().finish();
+            }
+        }else {
+            getActivity().finish();
+        }
+        bean = pointBeanDbUtil.getPointBeanByWPID(workId, pointId);
 
 
 
@@ -45,10 +96,45 @@ public class NewPointDetailFragment extends BaseToolbarFragment {
         View rootView = inflater.inflate(R.layout.new_fragment_point_detail, container, false);
         ButterKnife.bind(this, rootView);
         needTitle();
-
-
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //初始化小区门口及环境照
+
+
+        //设置页面标题
+        try {
+            int tempName = Integer.parseInt(bean.getDoor());
+            setTitle(tempName + "号楼" + (bean.getGround() == 0 ? "地下" : "地上") + "点位");
+        } catch (Exception e) {
+            setTitle(bean.getDoor() + (bean.getGround() == 0 ? "地下" : "地上") + "点位");
+        }
+        tvActionTime.setText("到画时间：" + bean.getPictime() + "    上画时间：" + bean.getWorktime());
+        //客户文字和照片
+        String cid = bean.getCid().trim();
+        for (PointListModel.ComListBean comBean : pointListModel.getComList()) {
+            if (comBean.getCid().trim().equals(cid)) {
+                tvCname.setText(comBean.getCname());
+                wbMemo.getSettings().setDefaultTextEncodingName("utf-8");
+                wbMemo.loadDataWithBaseURL("", comBean.getMemo(), "text/html", "utf-8", "");
+
+                if (comBean.getPicList() == null || comBean.getPicList().size() == 0) {
+                    ll_cust_photo.setVisibility(View.GONE);
+                } else {
+                    if(comBean.getPicList()!=null){
+                        ll_cust_photo.setCusImg(comBean.getPicList());
+                    }
+                }
+                break;
+            }
+        }
+
+    }
+
 
 
     @Override
--- app/src/main/java/cn/com/reachmedia/rmhandle/ui/view/Line3ImageLayout.java
+++ app/src/main/java/cn/com/reachmedia/rmhandle/ui/view/Line3ImageLayout.java
@@ -3,10 +3,23 @@ package cn.com.reachmedia.rmhandle.ui.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.com.reachmedia.rmhandle.R;
+import cn.com.reachmedia.rmhandle.model.PointListModel;
+import cn.com.reachmedia.rmhandle.ui.view.imagepager.ImageAllBean;
+import cn.com.reachmedia.rmhandle.utils.StringUtils;
+import cn.com.reachmedia.rmhandle.utils.ViewHelper;
 
 /**
  * Created by tedyuen on 16-9-20.
@@ -14,12 +27,58 @@ import cn.com.reachmedia.rmhandle.R;
 public class Line3ImageLayout extends FrameLayout {
 
 
+    @Bind(R.id.iv_cust_photo_1)
+    ImageView ivCustPhoto1;
+    @Bind(R.id.iv_cust_photo_2)
+    ImageView ivCustPhoto2;
+    @Bind(R.id.iv_cust_photo_3)
+    ImageView ivCustPhoto3;
+
+    private ImageView[] custPhotos;
+    List<ImageAllBean> imageDatas;//图片放大的资料
     public Line3ImageLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.line3_image_layout, this);
         ButterKnife.bind(this);
+        custPhotos = new ImageView[]{ivCustPhoto1,ivCustPhoto2,ivCustPhoto3};
+
+    }
 
 
+
+    /**
+     * 设置客户图片
+     * @param beanList
+     */
+    public void setCusImg(List<PointListModel.ComListBean.PicListBean> beanList) {
+        imageDatas = new ArrayList<>();
+        for (int i = 0; i < beanList.size(); i++) {
+            if (i >= 3) break;
+            PointListModel.ComListBean.PicListBean picBean = beanList.get(i);
+            //添加点击事件
+            ImageAllBean allBean = new ImageAllBean(picBean.getPicurlB(),ImageAllBean.URL_IMG);
+            imageDatas.add(allBean);
+            if (!StringUtils.isEmpty(picBean.getPicurlS())) {
+                custPhotos[i].setVisibility(View.VISIBLE);
+                Picasso.with(getContext()).load(picBean.getPicurlS()).placeholder(R.drawable.abc).resize(300,261).centerCrop().into(custPhotos[i]);
+            }
+        }
+
+    }
+
+    @OnClick(R.id.iv_cust_photo_1)
+    public void goViewCustomerPhoto1() {
+        ViewHelper.getAllImagePager(getContext(), imageDatas, 0);
+    }
+
+    @OnClick(R.id.iv_cust_photo_2)
+    public void goViewCustomerPhoto2() {
+        ViewHelper.getAllImagePager(getContext(), imageDatas, 1);
+    }
+
+    @OnClick(R.id.iv_cust_photo_3)
+    public void goViewCustomerPhoto3() {
+        ViewHelper.getAllImagePager(getContext(), imageDatas, 2);
     }
 
 }
